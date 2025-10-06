95ba008b3f893d09ae493b8b529a3ec074939394
NinjaBW <brian.patrick.wells@gmail.com>
2025-10-06 11:27:32 -0700

    Initial commit

---
+++ constants.py
@@ -0,0 +1,16 @@
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_KINDS = 3
+ASTEROID_SPAWN_RATE = 0.8  # seconds
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+'''While True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        pygame.display.flip()  # Update the display'''
